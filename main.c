3c7840b07d4cf1ae21d3e6dbe962ed81841744d7
4rjxn <131968160+4rjxn@users.noreply.github.com>
2026-01-19 22:03:05 +0530

    sand sim

---
+++ main.c
@@ -0,0 +1,107 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
+#include <stdio.h>
+
+#define WIDITH 1000
+#define HEIGHT 700
+#define GRID_SIZE 10
+static int grid[HEIGHT / GRID_SIZE][WIDITH / GRID_SIZE] = {0};
+
+void update() {
+  for (int y = (HEIGHT / GRID_SIZE - 2); y >= 0; y--) {
+    for (int x = 0; x < (WIDITH / GRID_SIZE); x++) {
+      if (grid[y][x] == 1) {
+        if (grid[y + 1][x] == 0) {
+          grid[y][x] = 0;
+          grid[y + 1][x] = 1;
+        } else if (grid[y + 1][x + 1] == 0) {
+          grid[y][x] = 0;
+          grid[y + 1][x + 1] = 1;
+        } else if (grid[y + 1][x - 1] == 0) {
+          grid[y][x] = 0;
+          grid[y + 1][x - 1] = 1;
+        }
+      }
+    }
+  }
+}
+void drawGrid(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+  for (int i = 0; i <= WIDITH - GRID_SIZE; i += GRID_SIZE) {
+    SDL_RenderDrawLine(renderer, i, 0, i, HEIGHT);
+  }
+  for (int i = 0; i <= HEIGHT - GRID_SIZE; i += GRID_SIZE) {
+    SDL_RenderDrawLine(renderer, 0, i, WIDITH, i);
+  }
+}
+
+void addSand(int x, int y) {
+  y = y / GRID_SIZE;
+  x = x / GRID_SIZE;
+  if (x >= 0 && x <= WIDITH / GRID_SIZE && y >= 0 && y <= HEIGHT / GRID_SIZE) {
+    grid[y][x] = 1;
+  }
+  // grid[(y / GRID_SIZE) - 1][x / GRID_SIZE] = 1;
+  // grid[(y / GRID_SIZE) - 1][(x / GRID_SIZE) + 1] = 1;
+  // grid[(y / GRID_SIZE) - 1][(x / GRID_SIZE) - 1] = 1;
+  // grid[(y / GRID_SIZE) + 1][(x / GRID_SIZE)] = 1;
+  // grid[(y / GRID_SIZE) + 1][(x / GRID_SIZE) + 1] = 1;
+  // grid[(y / GRID_SIZE) + 1][(x / GRID_SIZE) - 1] = 1;
+  // grid[(y / GRID_SIZE)][(x / GRID_SIZE) + 1] = 1;
+  // grid[(y / GRID_SIZE)][(x / GRID_SIZE) - 1] = 1;
+}
+
+int main() {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("init failed");
+    return 1;
+  }
+  SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
+  SDL_CreateWindowAndRenderer(WIDITH, HEIGHT, 0, &window, &renderer);
+  int running = 1;
+  SDL_Event event;
+  int isDown = 0;
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = 0;
+      } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+        isDown = 1;
+        int mx, my;
+        SDL_GetMouseState(&mx, &my);
+        addSand(mx, my);
+      } else if (event.type == SDL_MOUSEBUTTONUP) {
+        isDown = 0;
+      } else if ((event.type == SDL_MOUSEMOTION && isDown) || isDown) {
+        int mx, my;
+        SDL_GetMouseState(&mx, &my);
+        addSand(mx, my);
+      }
+    }
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    // drawGrid(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    for (int y = 0; y < (HEIGHT / GRID_SIZE); y++) {
+      for (int x = 0; x < (WIDITH / GRID_SIZE); x++) {
+        if (grid[y][x] == 1) {
+          SDL_Rect rect = {x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE};
+          SDL_RenderFillRect(renderer, &rect);
+        }
+      }
+    }
+    // for (int i = 0; i < 5; i++) {
+    update();
+    //}
+    SDL_RenderPresent(renderer);
+    SDL_Delay(16);
+  }
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
