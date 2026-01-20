341054a0717054827ac2a3ddd79c726d47a709c6
4rjxn <131968160+4rjxn@users.noreply.github.com>
2026-01-20 19:35:21 +0530

    multiple colors

--- main.c
+++ main.c
@@ -11,30 +11,32 @@
 #define HEIGHT 700
 #define GRID_SIZE 2
 static int grid[HEIGHT / GRID_SIZE][WIDITH / GRID_SIZE] = {0};
+int type = 1;
 
 void update() {
   for (int y = (HEIGHT / GRID_SIZE - 2); y >= 0; y--) {
     for (int x = 0; x < (WIDITH / GRID_SIZE); x++) {
-      if (grid[y][x] == 1) {
+      int stype = grid[y][x];
+      if (grid[y][x] != 0) {
         if (grid[y + 1][x] == 0) {
           grid[y][x] = 0;
-          grid[y + 1][x] = 1;
+          grid[y + 1][x] = stype;
 
         } else if (grid[y + 1][x + 1] == 0 && grid[y + 1][x - 1] == 0) {
           if (rand() % 2) {
             grid[y][x] = 0;
-            grid[y + 1][x + 1] = 1;
+            grid[y + 1][x + 1] = stype;
           } else {
             grid[y][x] = 0;
-            grid[y + 1][x - 1] = 1;
+            grid[y + 1][x - 1] = stype;
           }
 
         } else if (grid[y + 1][x + 1] == 0) {
           grid[y][x] = 0;
-          grid[y + 1][x + 1] = 1;
+          grid[y + 1][x + 1] = stype;
         } else if (grid[y + 1][x - 1] == 0) {
           grid[y][x] = 0;
-          grid[y + 1][x - 1] = 1;
+          grid[y + 1][x - 1] = stype;
         }
       }
     }
@@ -55,14 +57,16 @@ void addSand(int x, int y) {
   x = x / GRID_SIZE;
   int ofX;
   int ofY;
-  int sandQ = 10;
+  int sandQ = 20;
   for (int i = 0; i < sandQ; i++) {
     ofX = (rand() % 5) - 1;
     ofY = (rand() % 5) - 1;
     x = x + ofX;
     y = y + ofY;
     if (x >= 0 && x < WIDITH / GRID_SIZE && y >= 0 && y < HEIGHT / GRID_SIZE) {
-      grid[y][x] = 1;
+      if (grid[y][x] == 0) {
+        grid[y][x] = type;
+      }
     }
   }
 }
@@ -89,6 +93,13 @@ int main() {
         addSand(mx, my);
       } else if (event.type == SDL_MOUSEBUTTONUP) {
         isDown = 0;
+      } else if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_a) {
+          type += 1;
+        }
+        if (event.key.keysym.sym == SDLK_b) {
+          type -= 1;
+        }
       }
       //  else if ((event.type == SDL_MOUSEMOTION && isDown) || isDown) {
       //  int mx, my;
@@ -105,10 +116,16 @@ int main() {
     SDL_RenderClear(renderer);
     // drawGrid(renderer);
     // SDL_SetRenderDrawColor(renderer, 194, 178, 128, 53);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1);
     for (int y = 0; y < (HEIGHT / GRID_SIZE); y++) {
       for (int x = 0; x < (WIDITH / GRID_SIZE); x++) {
         if (grid[y][x] == 1) {
+          SDL_SetRenderDrawColor(renderer, 255, 40, 160, 255);
+        } else if (grid[y][x] == 2) {
+          SDL_SetRenderDrawColor(renderer, 40, 180, 255, 255);
+        } else if (grid[y][x] == 3) {
+          SDL_SetRenderDrawColor(renderer, 255, 255, 60, 255);
+        }
+        if (grid[y][x] != 0) {
           SDL_Rect rect = {x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE};
           SDL_RenderFillRect(renderer, &rect);
         }
